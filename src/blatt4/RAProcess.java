82267b8e482358fcff2da3bdd7f8fbcbd1084bfd
JeGa <jega@win>
2013-11-12 17:33:36 +0100

    Added comments with questions.

--- src/blatt4/RAProcess.java
+++ src/blatt4/RAProcess.java
@@ -19,11 +19,16 @@ public class RAProcess extends Process {
 		status = RessourceStatus.RELEASED;
 	}
 
+	// TRIGGER
 	public void process(RATrigger raTrigger) {
+		incLogicalTime(logicalTime);
+		
 		aquireLock();
 		process();
 		releaseLock();
 	}
+	
+	
 	private void aquireLock()
 	{
 		System.out.printf("%s trying to aquire lock...\n", this);
@@ -37,40 +42,16 @@ public class RAProcess extends Process {
 	{
 		status = RessourceStatus.WANTED;
 		wantedTime = logicalTime;
+		// Hier falsche Zeit bei 2 Threads die Trigger Msgs bearbeiten?
 	}
 	
 	private void waitForResponse()
 	{
 		RARequest req = new RARequest(this, wantedTime);
-		incLogicalTime(logicalTime);
+		// Problem bei mehreren Threads -> unnoetige Msgs?
 		latch = new CountDownLatch(destinations.size());
 		multicast(req);
 		waitLatch();
-	}
-	
-	private void holdLock()
-	{
-		status = RessourceStatus.HELD;
-		System.out.printf("%s holds lock.\n", this);
-	}
-	
-	private void releaseLock()
-	{
-		status = RessourceStatus.RELEASED;
-		System.out.printf("%s released lock.\n", this);
-	}
-	
-	private void process()
-	{
-		System.out.printf("%s processing ...\n", this);
-		try
-		{
-			TimeUnit.SECONDS.sleep(5);
-		}
-		catch (Exception e)
-		{
-			System.out.printf("%s %s\n",this, e.getMessage());
-		}
 	}
 	
 	private void waitLatch()
@@ -84,12 +65,37 @@ public class RAProcess extends Process {
 			System.out.println(this + ": " + e.getMessage());
 		}
 	}
+	
+	private void holdLock()
+	{
+		status = RessourceStatus.HELD;
+		System.out.printf("%s holds lock.\n", this);
+	}
+	
+	private void process()
+	{
+		System.out.printf("%s processing ...\n", this);
+		try
+		{
+			TimeUnit.SECONDS.sleep(3);
+		}
+		catch (Exception e)
+		{
+			System.out.printf("%s %s\n",this, e.getMessage());
+		}
+	}	
+	
+	private void releaseLock()
+	{
+		status = RessourceStatus.RELEASED;
+		System.out.printf("%s released lock.\n", this);
+	}
 
+	// REQUEST
 	public void process(RARequest raRequest) {
 		//System.out.printf("%s processing request from %s...\n", this, raRequest.getSender());
 		
 		incLogicalTime(raRequest.getLogicalTime());
-		//TODO zeiten vergleich stimmt noch nicht?
 		if(status == RessourceStatus.HELD ||
 		  (status == RessourceStatus.WANTED && wantedTime < raRequest.getLogicalTime()))
 			queueRequest(raRequest);
@@ -109,6 +115,7 @@ public class RAProcess extends Process {
 		destination.receiveMessage(response);
 	}
 
+	// RESPONSE
 	public void process(RAResponse raResponse) {
 		System.out.printf("%s received response from %s!\n",this, raResponse.getSender());
 		
